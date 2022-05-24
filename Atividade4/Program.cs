4fe24cea8d04171626b0843ebe2f6de17d1cecc0
Leonardo Belinato <70040882+haid3@users.noreply.github.com>
2022-05-24 00:44:36 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float celsius, far;
+            Console.WriteLine("Informe o Grau Celsius: ");
+            celsius = float.Parse(Console.ReadLine());
+            far = 9 * (celsius / 5) + 32;
+            Console.WriteLine("A quantidade de " + celsius + "°C equivale em Farhrenheit: " + far );
+            Console.Read();
+        }
+    }
+}
